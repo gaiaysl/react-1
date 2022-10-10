ec27714170e48fd3cdc5d1ff9288afd5719621ee
gaiaysl <gaye.ydg@gmail.com>
2022-10-10 15:23:11 +0300

    usestate useEffect example

---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+
+import classNames from "classnames"
+
+export default function Button({children,variant='default'}){
+   console.log(children)
+    return(
+        <>
+       <button className={classNames({
+        "p-4 h-10 flex items-center  rounded":true,
+        "bg-green-200":variant=='success',
+        "bg-red-500":variant=='danger',
+        "bg-yellow-200":variant=='warning',
+        "bg-gray-200":variant=='default',
+       })}>{children}</button> 
+        </>
+    )
+}
